7d392f34c680ac7007805d0b7ade3f4e29133568
Adi Malca <adimalca91@gmail.com>
2024-02-04 20:07:44 +0200

    Added a Room Model

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+We want to know when a room was first created and evey time it gets updated
+'''
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True) # null value is allowed
+    # participants = 
+    updated = models.DateTimeField(auto_now=True)        # Takes a snapshot on everytime we save this item.
+    created = models.DateTimeField(auto_now_add=True)    # Only takes a time stamp when we FIRST save or create this instance!
+
+    
+    def __str__(self):
+        return self.name
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Design with me'},
-    {'id':3, 'name':'Frontend developers'}
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn python!'},
+#     {'id':2, 'name':'Design with me'},
+#     {'id':3, 'name':'Frontend developers'}
+# ]
 
 def home(request):
+    rooms = Room.objects.all() # objects is the model Manager! A python way to communicate with the models / DB
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
@@ -17,9 +19,6 @@ def home(request):
 Working with path parameters
 '''
 def room(request, pk):
-    room = None
-    for room_dict in rooms:
-        if room_dict['id'] == int(pk):
-            room = room_dict
+    room = Room.objects.get(id=pk)   # Retrieves a single instance from the Room DB with the given id
     context = {'room':room}
     return render(request, 'base/room.html', context)
