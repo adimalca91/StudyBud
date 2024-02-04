2ab27dcc6d4acb5af2d77f5f3d6c489de014f12e
Adi Malca <adimalca91@gmail.com>
2024-02-04 11:27:41 +0200

    Added templates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'home.html')
 
 
 def room(request):
-    return HttpResponse("Room")
+    return render(request, 'room.html')
